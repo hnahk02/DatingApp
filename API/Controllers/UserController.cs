837c2ad6f078ebdd01116b9abb714adc8d1f467e
hnahk02 <100854712+hnahk02@users.noreply.github.com>
2023-10-31 16:18:41 +0700

    End of section 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/{controller}")] //api/user
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.AppUsers.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] //app/user/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.AppUsers.FindAsync(id);
+    }
+
+
+}
---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
